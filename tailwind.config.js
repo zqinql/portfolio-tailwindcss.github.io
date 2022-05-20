7c06654d6cb2cd2ff3ef9737911fc5752e103768
zqinql <zaqiinmtqn171206@gmail.com>
2022-05-20 23:45:07 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+    content: ['./index.html'],
+    theme: {
+        container: {
+            center: true,
+            padding: {
+                'xl': '10rem',
+            },
+        },
+        extend: {
+            colors: {
+                primary: '#14b8a6',
+                dark: '#0f172a',
+                secondary: '#64748b'
+
+            },
+            screens: {
+                'sm': '475px'
+            }
+
+
+        },
+    },
+    plugins: [],
+}
